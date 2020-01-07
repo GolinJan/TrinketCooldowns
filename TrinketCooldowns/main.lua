a1111ed65c7342692898a2789b8e397ae3451865
FrostAtom <vgolumbivskii@ukr.net>
2020-01-07 15:53:46 +0200

    added inspect and support of meta-gems\back enchants\rings

--- TrinketCooldowns/main.lua
+++ TrinketCooldowns/main.lua
@@ -1,29 +1,112 @@
 local SETTINGS = {
-	{unit = "player", points = {"CENTER",-400,-300}, size = 36, inArena = false, },
-	--{unit = "target", points = {"CENTER"}, size = 72,},
-	{unit = "party1", points = {"CENTER",-605,250}, size = 36, inArena = true, },
-	{unit = "party2", points = {"CENTER",-605,200}, size = 36, inArena = true, },
-	{unit = "arena1", points = {"CENTER",410,40}, size = 36, inArena = true, },
-	{unit = "arena2", points = {"CENTER",410,3}, size = 36, inArena = true, },
-	{unit = "arena3", points = {"CENTER",410,-35}, size = 36, inArena = true, },
+	{unit = "player", points = {"CENTER",-400,-300}, size = 36, onlyInArena = false, },
+	{unit = "party1", points = {"CENTER",-400,200}, size = 36, onlyInArena = true, },
+	{unit = "party2", points = {"CENTER",-400,150}, size = 36, onlyInArena = true, },
+	{unit = "arena1", points = {"CENTER",400,200}, size = 36, onlyInArena = true, onlyTrinkets = true, },
+	{unit = "arena2", points = {"CENTER",400,150}, size = 36, onlyInArena = true, onlyTrinkets = true, },
+	{unit = "arena3", points = {"CENTER",400,100}, size = 36, onlyInArena = true, onlyTrinkets = true, },
+
+	--{unit = "target", points = {"TOPLEFT",0,-50}},
+	--{unit = "focus", points = {"TOPLEFT",0,-90}},
 }
 
-local CreateFrame = CreateFrame
+local math,table = math,table
 local setmetatable = setmetatable
-local select,unpack,wipe = select,unpack,wipe
+local tonumber,select,unpack,wipe = tonumber,select,unpack,wipe
+local CreateFrame = CreateFrame
 local IsInInstance = IsInInstance
 local UnitAura,UnitExists,UnitGUID = UnitAura,UnitExists,UnitGUID
-local GetItemIcon = GetItemIcon
 local GetTime = GetTime
-local math = math
 local UIParent = UIParent
+local GetInventoryItemID = GetInventoryItemID
+local GetItemGem,GetItemInfo,GetItemIcon = GetItemGem,GetItemInfo,GetItemIcon
+local NotifyInspect = NotifyInspect
+local CanInspect = CanInspect
+local UnitCanAssist = UnitCanAssist
+
+local INVSLOT_BACK = INVSLOT_BACK
+local INVSLOT_HEAD = INVSLOT_HEAD
+
+local SLOT_TRINKET = 0x1
+local SLOT_RING = 0x2
+local SLOT_METAGEM = 0x3
+local SLOT_BACK = 0x4
 
 
-local spell2trinket,trinket2cooldown = unpack(select(2,...))
 local addon = CreateFrame("frame")
-local objects,tmpData = {},{}
-local trinket2uptime = {}
+local objects,cache = {},{}
+local spell2item,item2uptime,item2cooldown,item2slot = {},{},{},{}
+do
+	local namespace = select(2,...)
 
+	local item
+	for spell,data in pairs(namespace[1]) do
+		item = data[1]
+
+		if type(item) == "table" then
+			for i = 1,#item do
+				spell2item[spell] = item[i]
+			end
+		else
+			spell2item[spell] = item
+		end
+
+		item2cooldown[item] = data[2]
+		item2slot[item] = data[3]
+	end
+
+	namespace[1] = nil
+end
+
+
+local generateTrinketSlotHandler = function(slot)
+	return function(unit)
+		local id = GetInventoryItemID(unit,slot)
+		if id then
+			return id,GetItemIcon(id)
+		end
+	end
+end
+
+local generateRingSlotHandler = function(slot)
+	return function(unit)
+		local id = GetInventoryItemID(unit,slot)
+		if item2cooldown[id] then
+			return id,GetItemIcon(id)
+		end
+	end
+end
+
+local SLOTS = {
+	[1] = generateTrinketSlotHandler(INVSLOT_TRINKET1),
+	[2] = generateTrinketSlotHandler(INVSLOT_TRINKET2),
+	[3] = generateRingSlotHandler(INVSLOT_FINGER1),
+	[4] = generateRingSlotHandler(INVSLOT_FINGER2),
+	[5] = function(unit) -- helm meta
+		local id = GetInventoryItemID(unit,INVSLOT_HEAD)
+		if id then
+			local _,link = GetItemInfo(id)
+			if link then
+				_,link = GetItemGem(link,1)
+				if link then
+					id = tonumber(link:match("|Hitem:(%d+):"))
+					if item2cooldown[id] then
+						return id,GetItemIcon(id)
+					end
+				end
+			end
+		end
+	end,
+	[6] = function(unit) -- back enchant
+		local link = GetInventoryItemLink(unit,INVSLOT_BACK)
+		if link then
+			local id = tonumber(link:match("|Hitem:%d+:(%d+):"))
+			if id and item2cooldown[id] then
+				return id,GetItemIcon(link)
+			end
+		end
+	end,
+}
 
 local function UnitAuraBySpellID(unit,_spellId,filter)
 	local ret1,ret2,ret3,ret4,ret5,ret6,ret7,ret8,ret9,ret10,spellId
@@ -84,48 +167,64 @@ local iconsMT = {
 function framePrototype:Update()
 	local icons = self.icons
 
-	local data = tmpData[self.guid]
+	local data = cache[self.guid]
 	if data then
 		local unit = self.unit
+		local onlyTrinkets = self.onlyTrinkets
 
-		local icon,data_i,trinket
+		local icon,data_i
 		local texture,duration,expirationTime,_
-		for i = 1,math.max(#data,#icons) do
-			icon,data_i = icons[i],data[i]
+		for i = 1,(onlyTrinkets and 2 or math.max(#data,#icons)) do
+			icon = icons[i]
+
+			if onlyTrinkets then
+				data_i = nil
+				for j = 1,#data do
+					if item2slot[data[j].item] == SLOT_TRINKET then
+						i = i - 1
+						if i == 0 then
+							data_i = data[j]
+						end
+					end
+				end
+			else
+				data_i = data[i]
+			end
 
 			if data_i then
-				_,_,texture,_,_,duration,expirationTime = UnitAuraBySpellID(unit,data_i.spell)
-				trinket = spell2trinket[data_i.spell]
+				if data_i.spell then
+					_,_,texture,_,_,duration,expirationTime = UnitAuraBySpellID(unit,data_i.spell)
+				else
+					duration = nil
+				end
 
-				if duration and duration ~= 0 then
+				if duration and duration ~= 0 then -- uptime
 					icon.texture:SetTexture(texture)
 					icon.cooldown:SetCooldown(expirationTime-duration,duration)
 					icon.bg:SetTexture(0,0.8,0)
-					trinket2uptime[trinket] = duration
+					item2uptime[data_i.item] = duration
 				else
-					duration = trinket2cooldown[trinket]
+					icon.texture:SetTexture(data_i.texture)
 
-					icon.texture:SetTexture(GetItemIcon(trinket))
-					if duration ~= 0 and data_i.timestamp+duration > GetTime() then
-						local uptime = trinket2uptime[trinket]
+					duration = item2cooldown[data_i.item]
+					if duration and duration ~= 0 and data_i.timestamp and data_i.timestamp+duration > GetTime() then -- cooldown
+						local uptime = item2uptime[data_i.item]
 						if uptime and GetTime() > data_i.timestamp + uptime then
 							icon.cooldown:SetCooldown(data_i.timestamp+uptime,duration-uptime)
 						else
 							icon.cooldown:SetCooldown(data_i.timestamp,duration)
 						end
 						icon.bg:SetTexture(0.8,0,0)
-					else
+					else -- ready
 						icon.cooldown:Hide()
 						icon.bg:SetTexture(0,0.8,0)
 					end
 				end
-			else
-				icon.texture:SetTexture("Interface\\Icons\\INV_Misc_QuestionMark")
-				icon.cooldown:Hide()
-				icon.bg:SetTexture(0,0,0)
-			end
 
-			icon:Show()
+				icon:Show()
+			else
+				icon:Hide()
+			end
 		end
 	else
 		local icon
@@ -141,45 +240,94 @@ function framePrototype:Update()
 	end
 end
 
-function framePrototype:UpdateGUID()
-	self.guid = UnitGUID(self.unit)
-end
-
 function framePrototype:PLAYER_ENTERING_WORLD()
-	if self.inArena_event then
+	if self.onlyInArena_event then
 		if select(2,IsInInstance()) == "arena" then
-			self:RegisterEvent(self.inArena_event)
+			self:RegisterEvent(self.onlyInArena_event)
 		else
-			self:UnregisterEvent(self.inArena_event)
+			self:UnregisterEvent(self.onlyInArena_event)
 			self:Hide()
 			return
 		end
 	end
-	self:CheckUnitExists()
+
+	self:UpdateUnit()
 end
 
-function framePrototype:UNIT_INVENTORY_CHANGED(unit,slot)
-	if unit ~= self.unit then
+function framePrototype:UpdateUnit()
+	if not UnitIsPlayer(self.unit) then
+		self:Hide()
 		return
 	end
 
-	-- todo
-end
+	local guid = UnitGUID(self.unit)
+	if guid then
+		self.guid = guid
+		if not cache[guid] then
+			cache[guid] = {}
+		end
 
-function framePrototype:CheckUnitExists()
-	if UnitExists(self.unit) then
-		self:UpdateGUID()
 		self:Update()
+		self:Inspect()
 		self:Show()
 	else
 		self:Hide()
 	end
 end
 
+function framePrototype:Inspect()
+	self.toUpd = -1
+	self:SetScript("OnUpdate",self.OnUpdate_Inspect)
+end
+
+function framePrototype:OnHide()
+	self:SetScript("OnUpdate",nil)
+end
+
+function framePrototype:InspectInner()
+	local unit = self.unit
+	if not CanInspect(unit) then
+		return
+	end
+
+	NotifyInspect(unit)
+
+	local tbl = {}
+	local item,texture
+	for i = 1,#SLOTS do
+		item,texture = SLOTS[i](unit)
+		if item then
+			tbl[#tbl+1] = {item = item, texture = texture}
+		end
+	end
+	addon:SetCacheInfo(self.guid,tbl)
+	
+	self:SetScript("OnUpdate",nil)
+	self:Update()
+end
+
+function framePrototype:OnUpdate_Inspect(elapsed)
+	self.toUpd = self.toUpd - elapsed
+	if self.toUpd < 0 then
+		self.toUpd = 0.2
+
+		self:InspectInner()
+	end
+end
+
+function framePrototype:UNIT_INVENTORY_CHANGED(unit)
+	if unit ~= self.unit or not UnitCanAssist("player",unit) then
+		return
+	end
+
+	self:Inspect()	
+end
+
 
 function addon:CreateFrame()
 	local frame = setmetatable(CreateFrame("frame",nil,UIParent),frameMT)
 	frame:SetSize(12,12)
+	frame:SetScript("OnHide",frame.OnHide)
 	frame:SetScript("OnEvent",addon.OnEvent)
 
 	frame.icons = setmetatable({parent = frame},iconsMT)
@@ -191,6 +339,8 @@ end
 
 function addon:SetupFrame(frame,data)
 	frame.size = data.size or 36
+	frame.onlyTrinkets = data.onlyTrinkets
+
 	if data.points then
 		frame:SetPoint(unpack(data.points))
 	else
@@ -201,9 +351,7 @@ function addon:SetupFrame(frame,data)
 	frame.unit = unit
 
 	local event
-	if unit == "player" then
-		frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
-	elseif unit == "target" then
+	if unit == "target" then
 		event = "PLAYER_TARGET_CHANGED"
 	elseif unit == "focus" then
 		event = "PLAYER_FOCUS_CHANGED"
@@ -214,13 +362,15 @@ function addon:SetupFrame(frame,data)
 	end
 
 	if event then
-		if data.inArena then
-			frame.inArena_event = event
+		if data.onlyInArena then
+			frame.onlyInArena_event = event
 		else
 			frame:RegisterEvent(event)
 		end
-		frame[event] = frame.CheckUnitExists
+		frame[event] = frame.UpdateUnit
 	end
+
+	frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
 	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 end
 
@@ -235,18 +385,65 @@ function addon:UpdateObjectByGUID(guid)
 	end
 end
 
-function addon:AddTrinket(guid,spell,timestamp)
-	local data = tmpData[guid]
-	if data then
-		local added = false
-		local trinket = spell2trinket[spell]
-		local data_i
-		for i = 1,#data do
-			data_i = data[i]
+function addon:SetCacheInfo(guid,data)
+	local oldData = cache[guid]
+	
+	local data_i
+	for i = 1,#data do
+		data_i = data[i]
 
-			if spell2trinket[data_i.spell] == trinket then
-				data_i.spell = spell
-				data_i.timestamp = timestamp
+		if oldData then
+			local oldData_i
+			for i = 1,#oldData do
+				oldData_i = oldData[i]
+
+				if data_i.item == oldData_i.item then
+					data_i.spell = oldData_i.spell or data_i.spell
+					data_i.timestamp = oldData_i.timestamp or data_i.timestamp
+					break
+				end
+			end
+		end
+	end
+
+	cache[guid] = data
+end
+
+function addon:AlignGuidCache(guidCache,slot)
+	local alignTo = slot == SLOT_TRINKET and 2 or 1
+
+	local count = 0
+	local oldestTimestamp,oldestTimestamp_index = 0xFFFFFFFF
+	local guidCache_i,timestamp
+	for i = 1,#guidCache do
+		guidCache_i = guidCache[i]
+
+		if item2slot[guidCache_i.item] == slot then
+			count = count + 1
+
+			timestamp = guidCache_i.timestamp
+			if not timestamp or oldestTimestamp > timestamp then
+				oldestTimestamp = timestamp or 0xFFFFFFFF
+				oldestTimestamp_index = i
+			end
+		end
+	end
+
+	if count > alignTo then
+		table.remove(guidCache,oldestTimestamp_index)
+	end
+end
+
+function addon:SaveItemToCache(guid,spell,timestamp)
+ 	local item = spell2item[spell]
+ 	local tbl = {spell = spell, item = item, timestamp = timestamp, texture = GetItemIcon(item)}
+
+	local guidCache = cache[guid]
+	if guidCache then
+		local added = false
+		for i = 1,#guidCache do
+			if guidCache[i].item == item then
+				guidCache[i] = tbl
 
 				added = true
 				break
@@ -254,28 +451,14 @@ function addon:AddTrinket(guid,spell,timestamp)
 		end
 
 		if not added then
-			if #data >= 2 then
-				local olderTimestamp,olderTimestamp_i = 0xFFFFFFFF
-				local timestamp
-				for i = 1,#data do
-					timestamp = data[i].timestamp
-					if olderTimestamp > timestamp then
-						olderTimestamp = timestamp
-						olderTimestamp_i = i
-					end
-				end
-
-				data[olderTimestamp_i].spell = spell
-				data[olderTimestamp_i].timestamp = timestamp
-			else
-				data[#data+1] = {spell = spell, timestamp = timestamp}
-			end
+			guidCache[#guidCache+1] = tbl
 		end
-	else
-		tmpData[guid] = {{spell = spell, timestamp = timestamp}}
-	end
 
-	self:UpdateObjectByGUID(guid)
+		self:AlignGuidCache(guidCache,item2slot[item])
+		self:UpdateObjectByGUID(guid) -- object:UpdateUnit() creates cache[guid] field
+	else
+		cache[guid] = {tbl}
+	end
 end
 
 local watchedSubEvents = {
@@ -286,20 +469,20 @@ local watchedSubEvents = {
 }
 
 function addon:COMBAT_LOG_EVENT_UNFILTERED(_,subEvent,...)
-	if watchedSubEvents[subEvent] then
-		local srcGUID,spellId
-		srcGUID,_,_,_,_,_,spellId = ...
-		if spell2trinket[spellId] then
-			self:AddTrinket(srcGUID,spellId,GetTime())
-		end
+	if not watchedSubEvents[subEvent] then
+		return
+	end
+
+	local srcGUID,spellId
+	srcGUID,_,_,_,_,_,spellId = ...
+
+	if spell2item[spellId] then
+		self:SaveItemToCache(srcGUID,spellId,GetTime())
 	end
 end
 
 function addon:PLAYER_ENTERING_WORLD()
-	for i = 1,#objects do
-		objects[i]:CheckUnitExists()
-	end
-	wipe(tmpData)
+	wipe(cache)
 end
 
 function addon:OnEvent(event,...)
@@ -320,3 +503,5 @@ end
 addon:SetScript("OnEvent",addon.OnEvent)
 addon:Initialize()
 addon.Initialize = nil
+
+collectgarbage()
